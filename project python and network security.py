86ca722e69a63cfdcfd365726e70a3d25e02d5c1
kumaran0811 <86941541+kumaran0811@users.noreply.github.com>
2021-07-05 10:01:41 +0530

    Update project python and network security.py

--- project python and network security.py
+++ project python and network security.py
@@ -1,14 +1,14 @@
 import hashlib
-
+  
 # initializing string
-str_hash = "shapeai"
-
+str_hash = str(input("enter the name: "))
+  
 # encoding shapeai using encode()
 # then sending to md5(),sha256(),sha224()
 result1 = hashlib.md5(str_hash.encode())
-result2 = hashlib.sha256(str_hash.encode())
+result2 = hashlib.sha256(str_hash.encode()) 
 result3 = hashlib.sha224(str_hash.encode())
-
+ 
 # printing the equivalent hexadecimal value.
 print(result1.hexdigest())
 print(result2.hexdigest())
